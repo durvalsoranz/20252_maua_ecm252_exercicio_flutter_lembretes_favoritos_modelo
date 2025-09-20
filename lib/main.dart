53a692ee0e78cb667ed8bcbcd09ebd393c46f44c
durvalsoranz <durvalsoranz@gmail.com>
2025-09-20 11:16:29 -0300

    feat: exibe lista ficticia

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Meus Lembretes', home: const InputWidget());
+    return MaterialApp(
+      title: 'Meus Lembretes',
+      home: const Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Expanded(child: ListaLembretes()),
+              InputWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
 
@@ -23,21 +37,38 @@ class InputWidget extends StatefulWidget {
 class _InputWidgetState extends State<InputWidget> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Digite seu novo lembrete',
-              ),
-            ),
-          ],
-        ),
+    return TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: 'Digite seu novo lembrete',
       ),
+    );
+  }
+}
+
+class ListaLembretes extends StatelessWidget {
+  const ListaLembretes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final lembretesFicticios = [
+      'Preparar aula de programação',
+      'Fazer feira',
+      'Preparar marmitas',
+    ];
+
+    return ListView.builder(
+      itemCount: lembretesFicticios.length,
+      itemBuilder: (context, index) {
+        final lembrete = lembretesFicticios[index];
+        return Card(
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(lembrete),
+          ),
+        );
+      },
     );
   }
 }
